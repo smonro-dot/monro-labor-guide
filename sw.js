a35fe19477c8f21e883c1dc48944ca82531008c5
smonro-dot <smonro@roofmaxx.com>
2026-09-11 19:37:18 -0700

    Put original sheet pictures on the labor guide app

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'monro-sheets-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-180.png',
+  './labor-1.html','./labor-2.html','./labor-3.html',
+  './comfort-1.html','./comfort-2.html',
+  './space-1.html','./space-2.html',
+  './plan-1.html','./plan-2.html'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
+});
